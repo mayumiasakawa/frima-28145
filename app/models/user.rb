0a24d7ea7c07964955cca997db2bdb1296c2760e
mayumiasakawa <m.a.mayumi214@gmail.com>
2020-10-27 23:22:19 +0900

    ログインの有無での表記変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    validates :nickname, presence: true
+    validates :password, length: { minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/}, confirmation: true
+    validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,message: "is invalid. Format is not."}
+    validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :first_name, presence: true,  format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :last_name_kana, presence: true,  format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :first_name_kana, presence: true,  format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :birthday, presence: true
 end

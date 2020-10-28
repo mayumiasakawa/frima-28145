be9c95c9e5a1a6d87425e5fb8485d914effc84f9
mayumiasakawa <m.a.mayumi214@gmail.com>
2020-10-28 14:52:29 +0900

    factorybot再導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
-  end
+    first_name          {"山田"}
+    last_name           {"花子"}
+    first_name_kana     {"ヤマダ"}
+    last_name_kana      {"ハナコ"}
+    nickname            {"yamada"}
+    birthday            {"19300101"}
+    email               {"kkk@gmail.com"}
+    password            {"aaa111"}
+    encrypted_password  {password}
+  end 
 end

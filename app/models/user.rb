3a58ea4f37a360288cbe1f4e78b7de7fe033cfc9
mayumiasakawa <m.a.mayumi214@gmail.com>
2020-10-28 18:18:03 +0900

    rubocop導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    validates :nickname, presence: true
-    validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }, confirmation: true
-    validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,message: "is invalid. Format is not."}
-    validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :first_name, presence: true,  format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :last_name_kana, presence: true,  format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-    validates :first_name_kana, presence: true,  format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-    validates :birthday, presence: true
+  validates :nickname, presence: true
+  validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }, confirmation: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: 'is invalid. Format is not.' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+  validates :birthday, presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    first_name          {"山田"}
-    last_name           {"花子"}
-    first_name_kana     {"ヤマダ"}
-    last_name_kana      {"ハナコ"}
-    nickname            {Faker::Name.initials(number: 2)}
-    birthday            {Faker::Date.birthday}
-    email               {Faker::Internet.free_email}
-    password            {Faker::Internet.password(min_length: 6)}
-    encrypted_password  {password}
-  end 
+    first_name          { '山田' }
+    last_name           { '花子' }
+    first_name_kana     { 'ヤマダ' }
+    last_name_kana      { 'ハナコ' }
+    nickname            { Faker::Name.initials(number: 2) }
+    birthday            { Faker::Date.birthday }
+    email               { Faker::Internet.free_email }
+    password            { Faker::Internet.password(min_length: 6) }
+    encrypted_password  { password }
+  end
 end

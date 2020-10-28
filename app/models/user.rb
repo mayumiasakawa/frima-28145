b4de703da1093e4fa69ba4543ae2397d3194878c
mayumiasakawa <m.a.mayumi214@gmail.com>
2020-10-28 18:55:08 +0900

    バリデーション with_optionでまとめる

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,17 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
   VALID_NAMEKANA_REGEX = /\A[ァ-ヶー－]+\z/
+
+  with_options presence: true, format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters.' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: VALID_NAMEKANA_REGEX, message: 'is invalid. Input full-width katakana characters.' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
   validates :nickname, presence: true
   validates :password, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }, confirmation: true
   validates :email, presence: true, uniqueness: true, format: { with:VALID_EMAIL_REGEX, message: 'is invalid. Format is not.' }
-  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters.' }
-  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters.' }
-  validates :last_name_kana, presence: true, format: { with: VALID_NAMEKANA_REGEX, message: 'is invalid. Input full-width katakana characters.' }
-  validates :first_name_kana, presence: true, format: { with: VALID_NAMEKANA_REGEX, message: 'is invalid. Input full-width katakana characters.' }
   validates :birthday, presence: true
 end

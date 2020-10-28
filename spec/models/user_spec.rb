d11e1779d82125e2c350501f53e5c7a07bc08e56
mayumiasakawa <m.a.mayumi214@gmail.com>
2020-10-28 14:04:30 +0900

    エラー改善　
     bundle exec rspec spec/models/user_spec.rb　エラー

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,33 @@ RSpec.describe User, type: :model do
     end
     it "emailが空では登録できない" do
     end
+    it "emailが一意性でないければ登録できない" do
+    end
+    it "passwordが空では登録できない" do
+    end
+    it "passwordが5文字以下では登録できない" do
+    end
+    it "passwordが半角英数字が入っていなければ登録できない" do
+    end
+    it "encrypted_passwordが2回入力がなければ登録できない" do
+    end
+    it "first_nameが空だと登録できない" do
+    end
+    it "last_nameが空だと登録できない" do
+    end
+    it "first_nameが全角でなければ登録できない" do
+    end
+    it "last_nameが全角でなければ登録できない" do
+    end
+    it "first_name_kanaが空だと登録できない" do
+    end
+    it "last_name_kanaが空だと登録できない" do
+    end
+    it "first_name_kanaが全角でなければ登録できない" do
+    end
+    it "last_name_kanaが全角でなければ登録できない" do
+    end
+    it "birthdayが空だと登録できない" do
+    end
   end
 end

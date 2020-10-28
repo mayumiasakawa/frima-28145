77e5938fa27258fc253317ee21fe6f704ab4cf00
mayumiasakawa <m.a.mayumi214@gmail.com>
2020-10-28 14:55:12 +0900

    Faker導入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  describe 'ユーザー新規登録' do
+    it "nicknameが空だと登録できない" do
+      @user.nickname = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")      
+    end
+    it "emailが空では登録できない" do
+      @user.email = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")   
+    end
+    it "emailが一意性でないければ登録できない" do
+    end
+    it "passwordが空では登録できない" do
+      @user.password = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")   
+    end
+    it "passwordが5文字以下では登録できない" do
+    end
+    it "passwordが半角英数字が入っていなければ登録できない" do
+    end
+    it "encrypted_passwordが2回入力がなければ登録できない" do
+    end
+    it "first_nameが空だと登録できない" do
+      @user.first_name = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name can't be blank")   
+    end
+    it "last_nameが空だと登録できない" do
+      @user.last_name = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name can't be blank")   
+    end
+    it "first_nameが全角でなければ登録できない" do
+    end
+    it "last_nameが全角でなければ登録できない" do
+    end
+    it "first_name_kanaが空だと登録できない" do
+      @user.first_name_kana = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name kana can't be blank")   
+    end
+    it "last_name_kanaが空だと登録できない" do
+      @user.last_name_kana = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name kana can't be blank")   
+    end
+    it "first_name_kanaが全角でなければ登録できない" do
+    end
+    it "last_name_kanaが全角でなければ登録できない" do
+    end
+    it "birthdayが空だと登録できない" do
+      @user.birthday = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birthday can't be blank")   
+    end
+  end
 end

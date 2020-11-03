5cd99eef294ae9f9daafea213b72639fb36b9fee
mayumiasakawa <63479656+mayumiasakawa@users.noreply.github.com>
2020-11-03 17:36:39 +0900

    Revert "商品一覧表示機能"

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :delete]
+  before_action :authenticate_user!, only: [:new]
   
   def index
-    @items = Item.all.includes(:user).order("created_at DESC")
   end
 
   def new

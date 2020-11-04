0e65a127cba083e6af59b6ce7071b3489299c596
mayumiasakawa <m.a.mayumi214@gmail.com>
2020-11-04 12:55:01 +0900

    ビューファイル、コントローラーファイル変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :delete]
-  
+  before_action :set_item, only: [:show,:edit]
+
   def index
     @items = Item.all.includes(:user).order("created_at DESC")
   end
@@ -19,13 +20,25 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
-    end
+  end
+
+  def edit
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to item_path
+  end
   
   private
   
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :status_id, :price, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end

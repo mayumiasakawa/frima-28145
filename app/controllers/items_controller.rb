da6887e6c671e68f9e5e28c3751c5f7255a681d2
mayumiasakawa <m.a.mayumi214@gmail.com>
2020-11-04 11:53:08 +0900

    コントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,15 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+
+  def show
+    @item = Item.find(params[:id])
+    @category = Category.find_by(id: @item.category_id)
+    @status = Status.find_by(id: @item.status_id)
+    @shipping_fee = ShippingFee.find_by(id: @item.shipping_fee_id)
+    @prefecture = Prefecture.find_by(id: @item.prefecture_id)
+    @scheduled_delivery = ScheduledDelivery.find_by(id: @item.scheduled_delivery_id)
+  end
   
   private
   

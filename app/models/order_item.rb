b18ad342ae38d85d77eac8d500b3aad6ce6b4004
mayumiasakawa <m.a.mayumi214@gmail.com>
2020-11-16 17:27:12 +0900

    shipping変更

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -12,7 +12,7 @@ class OrderItem
     end
 
   def save
-    Order.create(item_id: item_id, user_id: user_id)
-    Shipping.create(postal_code: postal_code, prefecture: prefecture,city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order_id)
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Shipping.create(postal_code: postal_code, prefecture: prefecture,city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end

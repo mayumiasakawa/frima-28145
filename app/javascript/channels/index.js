42832c21b4128359c4eef113cc04efd366da1565
mayumiasakawa <m.a.mayumi214@gmail.com>
2020-12-29 12:03:42 +0900

    new viewfile create

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,17 +3,3 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
-
-window.addEventListener('load', function(){
-  const inputPrice = document.getElementById("item-price");
-  if (inputPrice != null){
-  inputPrice.addEventListener('input',function(){
-    const addTaxPrice = inputPrice.value * 0.1;
-    const profitPrice = (inputPrice.value - addTaxPrice);
-    const addTax = document.getElementById("add-tax-price");
-    const profit = document.getElementById("profit");
-    addTax.innerHTML = `${addTaxPrice}`;
-    profit.innerHTML = `${profitPrice}`;
-  })
-}
-})

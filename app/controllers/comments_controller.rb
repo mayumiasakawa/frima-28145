abe1ce65474d8be4dafe74ec9f51ba7cea516c26
mayumiasakawa <m.a.mayumi214@gmail.com>
2020-12-29 13:04:08 +0900

    comments_controller edit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,9 +5,16 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = Comment.new(text: params[:comment][:text])
+    # @comment = Comment.new(text: params[:comment][:text])
+    @comment = Comment.new(comment_params)
     if @comment.save
       ApplicationCable.server.boradcast 'comment_channel', content: @comment
+      redirect_to "/items/#{comment.item.id}"
     end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user_id, item_id: params[:item_id])
   end
 end
